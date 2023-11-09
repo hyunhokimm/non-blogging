d2de87936d47b2091fb83c1198afa35b88463fa2
hyeoonjeoong <jeooooong22@gmail.com>
2023-11-09 16:19:14 +0900

    login

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.render("login.ejs");
 });
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
 
 app.listen(port, function () {
   console.log(`Server OPEN: ${port}`);
