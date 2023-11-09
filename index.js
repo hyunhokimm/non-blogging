0a72f727094ee467eb2c0864d44abd48cac163e8
Siri Lee <eunsilling@gmail.com>
2023-11-09 15:56:33 +0900

    ✨ port Open

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.listen(port, function () {
+  console.log(`Server OPEN: ${port}`);
+});
