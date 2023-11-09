85b09808dd7bd98c50179e21de1233d685b11ade
Siri Lee <eunsilling@gmail.com>
2023-11-09 18:25:06 +0900

    🐛 Conflict 해결

--- model/index.js
+++ model/index.js
@@ -1,19 +1,1 @@
-function User(Sequelize, DataTypes) {
-  return Sequelize.define(
-    "user",
-    {
-      email: {
-        type: Datatypes.STRING(25),
-        allowNull: False,
-      },
-      password: varchar,
-    },
-    {
-      tableName: "user",
-      freezeTableName: true,
-      timestamps: false,
-    }
-  );
-}
 
-module.exports = Customer;
