978bf21a02a53b51ddfc3bc86fe798b247eaa421
Siri Lee <eunsilling@gmail.com>
2023-11-09 18:52:07 +0900

    ✨ 필요없는 코드 삭제

--- controller/Clogin.js
+++ controller/Clogin.js
@@ -5,29 +5,15 @@ exports.index = (req, res) => {
   res.render("login");
 };
 
-exports.signin = (req, res) => {
+exports.login = (req, res) => {
   res.render("signin");
 };
-exports.postSignin = (req, res) => {
+exports.postLogin = (req, res) => {
   const { email, password } = req.body;
   const user = User.findOne({ where: { email, password } });
   if (user) {
-    // req.cookie.user = user;
     res.send(user);
   } else {
     res.status(401).send({ msg: "Login Failed" });
-  }
-};
-
-exports.profile = (req, res) => {
-  const id = req.headers.cookie.split("=")[1];
-  const user = User.findOne({
-    where: { id: id },
-  });
-
-  if (user) {
-    res.render("profile", { data: user });
-  } else {
-    res.redirect("/user/signup");
   }
 };
