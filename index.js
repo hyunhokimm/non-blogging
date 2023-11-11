88a007a58f548b982b5c755e23427aeeb2b6b570
Siri Lee <eunsilling@gmail.com>
2023-11-11 16:56:12 +0900

    🐛 edit error code

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ const router = require("./routes");
 const app = express();
 const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.set("view engine", "ejs");
 
-app.use(express.static( 'static'))
-app.use('/',router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("static"));
+app.use("/", router);
 
-app.get("/", (req, res) => {
-  res.render("login.ejs");
-});
+// app.get("/", (req, res) => {
+//   res.render("login.ejs");
+// });
 
-app.get("/join", (req, res) => {
-  res.render("signUp.ejs");
-});
+// app.get("/join", (req, res) => {
+//   res.render("signUp.ejs");
+// });
 
 app.get("/blog", (req, res) => {
   res.render("blogging.ejs");
 });
 
-app.post("/post", (req, res) => {
-  console.log(req.body.form)
-});
-
+// app.post("/post", (req, res) => {
+//   console.log(req.body.form)
+// });
 
 app.get("/myblog", (req, res) => {
   res.render("myblog.ejs");
 });
 
-app.get("/allblog", (req, res) => {
-  res.render("allblog.ejs");
-});
+// app.get("/allblog", (req, res) => {
+//   res.render("allblog.ejs");
+// });
 
 app.listen(port, function () {
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
