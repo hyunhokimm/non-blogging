20ee0f618e8754a3d6a4f7b0cdd59042e9497329
Siri Lee <eunsilling@gmail.com>
2023-11-13 22:03:48 +0900

    ♻️ update Chome Clogin Cmyblog

--- controller/Chome.js
+++ controller/Chome.js
@@ -1,8 +1,8 @@
-const { userTable } = require("../model/index");
+const { userBlogs } = require("../model");
 
 // allblog page
 exports.home = (req, res) => {
-  const allblog = userTable.findAll({
+  const allblog = userBlogs.findAll({
     attributes: ["id", "nickname"],
     // 또같은 블로그만 불러와지니 배열로 랜덤방식! sort방식 검색해보자~~
   });
