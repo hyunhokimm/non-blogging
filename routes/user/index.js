da08c7a8bc8779908f9cd187e3dde38a8d77b69f
Siri Lee <eunsilling@gmail.com>
2023-11-13 22:05:32 +0900

    ♻️ update routes/note routes/user

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const userRoute = express.Router();
-const {login,postLogin} = require("../../controller/Clogin");
-const {signUpPage,signUpProcess} = require("../../controller/Csignup");
-
+const { login, isLogin } = require("../../controller/Clogin");
+const { signUpPage, signUpProcess } = require("../../controller/Csignup");
 
 // 로그인
 userRoute.get("/login", login);
-userRoute.post("/login", postLogin);
+userRoute.post("/login", isLogin);
 
 // 회원가입
 userRoute.get("/signup", signUpPage);
 userRoute.post("/signup", signUpProcess);
 
 //회원정보
-userRoute.get('/myinfo',(req,res)=>{
-    res.render('myInfo')
-})
+userRoute.get("/myinfo", (req, res) => {
+  res.render("myInfo");
+});
 
-
-module.exports = userRoute
+module.exports = userRoute;
