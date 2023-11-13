428dcc8ee500a1926274533ad0c29c9d0a416e38
hyunhokimm <whoaukim@gmail.com>
2023-11-13 20:10:25 +0900

    ✅ 프론트와 백엔드 연결

--- routes/note/index.js
+++ routes/note/index.js
@@ -4,19 +4,18 @@ const noteRoute = express.Router();
 // const {postPage,uploadPostProcess} = require("../../controller/Cblogging");
 
 // 모든 블로그
-noteRoute.get("/allblog", (req,res)=>{
-    res.render('allblog')
+noteRoute.get("/allblog", (req, res) => {
+  res.render("allblog");
 });
 
 // 나의 게시글들
-noteRoute.get("/", (req, res)=>{
-    res.render('notebook')
+noteRoute.get("/", (req, res) => {
+  res.render("notebook");
 });
 
 //나의 블로그 작성
-noteRoute.get("/write", (req, res)=>{
-    res.render('write')
+noteRoute.get("/write", (req, res) => {
+  res.render("write");
 });
 
-
-module.exports= noteRoute
+module.exports = noteRoute;
