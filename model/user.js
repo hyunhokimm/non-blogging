a6b549950ced8afc825a18624d7acd3e9939f42c
Siri Lee <eunsilling@gmail.com>
2023-11-14 10:45:26 +0900

    ✅ model 폴더명 변경

--- model/user.js
+++ model/user.js
@@ -1,36 +1,36 @@
-function userTable(Sequelize, DataTypes){
-    return Sequelize.define(
-      "user", 
-      { 
-        // id 기본키로 설정
-        id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true
-        }, 
-        // 이메일 설정
-        email: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        },
-        // 닉네임 설정
-        nickname: {
-            type: DataTypes.STRING(30),
-            allowNull: false
-        },
-        // 비밀번호 설정 (솔트 미적용)
-        password: {
-            type: DataTypes.STRING(100),
-            allowNull: false
-        }
-      }, 
-      { 
-        tableName: "user",
-        freezeTableName : true, 
-        timestamps: false
-      } 
-    );
-  }
-  
-  module.exports=userTable;
+function userTable(Sequelize, DataTypes) {
+  return Sequelize.define(
+    "user",
+    {
+      // id 기본키로 설정
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      // 이메일 설정
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      // 닉네임 설정
+      nickname: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      // 비밀번호 설정 (솔트 미적용)
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "user",
+      freezeTableName: true,
+      timestamps: false,
+    }
+  );
+}
+
+module.exports = userTable;
