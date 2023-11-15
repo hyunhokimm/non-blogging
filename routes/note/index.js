2a4d88cf72a7bf1199955ae367cce9d369b3334e
Siri Lee <eunsilling@gmail.com>
2023-11-15 18:27:01 +0900

    🎨  note 상세페이지,수정 및 삭제 코드 정리

--- routes/note/index.js
+++ routes/note/index.js
@@ -32,24 +32,12 @@ noteRoute.get("/write", (req, res) => {
 // noteRoute.post("/notebook/write", write);
 // });
 
-// 게시물 상세 페이지 - 컨트롤러 구현 전
-// router.get("/notebook/:noteId", async (req, res, next) => {
-//   try {
-//     const { noteId } = req.params;
-//     const post = await Post.findOne({ noteId }); // 게시글 검색
-//     if (!post) {
-//       throw new Error("Post NotFound");
-//     }
-
-//     res.render("/notebook/:noteId" <<<<<????이게 맞나?, { post });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+// 게시물 상세 페이지
+router.get("/notebook/:noteId", note);
 
 // 게시글에 작성자 연동
 // router.get("/", async (req, res) => {
-//   const posts = await Post.find({})
+//   const notebook = await notebook.find({})
 //     .sort({ createdAt: -1 })
 //     .skip(perPage * (page - 1))
 //     .limit(perPage)
@@ -58,41 +46,18 @@ noteRoute.get("/write", (req, res) => {
 // });
 
 // // 수정및삭제 작성자 확인
-// const post = await Post.find({
+// const note = await notebook.find({
 //   noteId,
 // }).populate("author");
 
-// if (post.author.shortId !== req.user.shortId) {
+// if (post.author.noteId !== req.user.noteId) {
 //   throw new Error("Not Authorized");
 // }
 
 // 게시물 수정 페이지
-// router.post("/:noteId", async (req, res, next) => {
-//   const { noteId } = req.params;
-//   const { title, content } = req.body;
-//   const post = await Post.findOneAndUpdate(
-//     { noteId },
-//     {
-//       title,
-//       content,
-//     }
-//   ); // noteId 검색해서 내용을 title, content로 변경
-//   if (!post) {
-//     throw new Error("Post NotFound");
-//   }
-
-//   res.rendirect(`/notebook/${noteId}`, { post });
-// });
+router.post("/notebook/:noteId", editNote);
 
 // 게시물 삭제 기능
-// router.delete("/notebook/:noteId", async (req, res, next) => {
-//   const { noteId } = req.params;
-//   try {
-//     await Post.delete({ noteId });
-//     res.send("Success Delete");
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+router.delete("/notebook/:noteId", deleteNote);
 
 module.exports = noteRoute;
