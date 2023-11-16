fd8ae958f1f9044b26369537268a4adb13c4781a
KrillM <mks160615@gmail.com>
2023-11-16 19:30:09 +0900

    ✨ Feat :  Cmyblog 생성

--- controller/Cmyblog.js
+++ controller/Cmyblog.js
@@ -1,21 +1,36 @@
-const notebook = require("../model");
-const user = require("../model");
+const { user, notebook } = require("../model");
 
-// 나의 게시글들
-exports.notebook = (req, res) => {
-  res.render("notebook");
-};
+// 사용자 페이지
+exports.userPage = (req, res) => {
+  const email = req.body.email;
 
-// 나의 블로그 작성
-exports.write = (req, res) => {
-  // let userId = notebook();
-  // let email = user();
-
-  if (req.params.userId == req.params.email) {
-    // ??기존유저
-    res.render("write");
-    // 신규유저
-  } else {
-    res.render("write");
+  if (!req.session.user) {
+    res.redirect("/");
+    return false;
   }
+
+  user.findOne({
+    where: {
+        email: email,
+    }
+  }).then((userResult) => {
+    if (userResult) {
+      notebook.findAll({
+        where: {
+          currentUser: email,
+        }
+      }).then((noteResult) => {
+        console.log("조회 ", userResult, noteResult);
+        res.render("notebook", { user: userResult, note: noteResult });
+      }).catch((noteErr) => {
+        console.log(noteErr);
+        res.status(500).send("접근 오류 발생");
+      });
+    } else {
+      res.render("signUp");
+    }
+  }).catch((userErr) => {
+    console.log(userErr);
+    res.status(500).send("접근 오류 발생");
+  });
 };
