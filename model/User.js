2a6428fa0e1f02ff43737d2820dda5f43fd88af2
hyunhokimm <whoaukim@gmail.com>
2023-11-18 15:05:39 +0900

    ✅ login 페이지 post

--- model/User.js
+++ model/User.js
@@ -2,17 +2,11 @@ function user(Sequelize, DataTypes) {
   return Sequelize.define(
     "user",
     {
-      // id > auto로 설정
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
       // 이메일
       email: {
         type: DataTypes.STRING(50),
         allowNull: true,
+        primaryKey: true,
       },
       // 닉네임
       nickname: {
@@ -26,10 +20,6 @@ function user(Sequelize, DataTypes) {
       },
       // checkPassword 부분은 필요 없을 듯 합니다.
       // 프론트 에서 2개의 비밀 번호 검증 후 password만 보내주거든요
-      checkPassword: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
-      },
     },
     {
       tableName: "User",
