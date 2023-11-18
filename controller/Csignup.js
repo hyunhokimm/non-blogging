c88cd8976113eb97db7831ab6b0ee9dcf80ddd45
KrillM <mks160615@gmail.com>
2023-11-18 17:49:13 +0900

    ♻️ Refactor :  중복 및 생략된 부분 해결

--- controller/Csignup.js
+++ controller/Csignup.js
@@ -1,10 +1,7 @@
-
 const { user } = require("../model");
 const crypto = require("crypto");
 
-
-
-exports.signUpPage = (req, res) => {
+exports.signup = (req, res) => {
   res.render("signup");
 }
 
@@ -29,7 +26,7 @@ exports.signupProcess = async (req, res) => {
         nickname,
       })
       .then(() => {
-        // return res.render("signup");
+        res.render("signUp");
       })
       .catch(function (err) {
         console.log(err);
@@ -43,29 +40,6 @@ exports.signupProcess = async (req, res) => {
 
 function hashPassword(password) {
   const salt = crypto.randomBytes(16).toString("hex");
-  const hash = crypto
-    .pbkdf2Sync(password, salt, 1000, 64, "sha512")
-
-    .toString("hex");
-
-exports.signUpProcess = (req, res) => {
-  const data = {
-    email: req.body.email,
-    // 비밀번호 해쉬로 암호화
-    password: hashPassword(req.body.password),
-    nickname: req.body.nickname
-  }
-  User.create(data).then((result) => {
-    res.send(result);
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send("등록 오류 발생");
-  })
-}
-
-function hashPassword(password) {
-  const salt = crypto.randomBytes(16).toString("hex");
   const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex");
-
   return `${salt}:${hash}`;
 }
