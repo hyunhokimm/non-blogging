80bffc1237185bfa487037a5c036207638c54eb1
hyunhokimm <whoaukim@gmail.com>
2023-11-18 16:29:31 +0900

    ✅ signup 페이지

--- controller/Csignup.js
+++ controller/Csignup.js
@@ -1,4 +1,5 @@
 const { user } = require("../model");
+const crypto = require("crypto");
 
 // 회원가입 페이지
 exports.signup = (req, res) => {
@@ -6,19 +7,31 @@ exports.signup = (req, res) => {
 };
 
 // 회원가입 진행
-exports.signupProcess = (req, res) => {
-  console.log(req.body);
-  const data = {
-    email: req.body.email,
-    nickname: req.body.nickname,
-  };
+exports.signupProcess = async (req, res) => {
+  const { email, password, nickname } = req.body;
+  const result = await user.findOne({ email });
+  console.log(result);
+  const hashpassword = hashPassword(password);
+  console.log("hashpass", hashpassword);
   user
-    .create(data)
+    .create({
+      email,
+      password: hashpassword,
+      nickname,
+    })
     .then((result) => {
-      res.send(result);
+      return res.render("signup");
     })
     .catch(function (err) {
       console.log(err);
       res.status(500).send("등록 오류가 발생하였습니다.");
     });
 };
+
+function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hash = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+    .toString("hex");
+  return `${salt}:${hash}`;
+}
