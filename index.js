7c9d9d89b798e6afeb538ef451f7c2f2ebb14973
KrillM <mks160615@gmail.com>
2023-11-21 23:45:03 +0900

    ♻️ Refactor :  시큐어 정정

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use(
       httpOnly: true, // document.cookie로는 접속x
       maxAge: 60 * 60 * 24,
     },
-    secure: false, // https에서만 동작하도록 함
+    secure: true, // https에서만 동작하도록 함
     store: new FileStore({
-      path: path.join(__dirname, "/sessions"),
+      path: "/sessions",
       retries: 2, // 시도 횟수
       retriesTimeout: 1000, // 재시도 간격 (밀리초)
     }), // nodejs 재시작이 되어도 세션 값 유지
