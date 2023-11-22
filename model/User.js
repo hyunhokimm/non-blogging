a5d40b56718971d4a1dc402742b9057b8e42f7a6
Siri Lee <eunsilling@gmail.com>
2023-11-22 22:19:53 +0900

    🗃 DB column 과 model file code 동일시 작업

--- model/User.js
+++ model/User.js
@@ -4,19 +4,19 @@ function user(Sequelize, DataTypes) {
     {
       // 이메일
       email: {
-        type: DataTypes.STRING(50),
-        allowNull: true,
-        primaryKey: true,
-      },
-      // 닉네임
-      nickname: {
         type: DataTypes.STRING(30),
-        allowNull: true,
+        allowNull: false,
+        primaryKey: true,
       },
       // 비밀번호
       password: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      // 닉네임
+      nickname: {
+        type: DataTypes.STRING(15),
+        allowNull: false,
       },
     },
     {
